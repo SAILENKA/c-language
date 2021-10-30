ce2c4e8c78157f09730958e415f828e5d9c58db5
SAILENKA <87110724+SAILENKA@users.noreply.github.com>
2021-10-30 10:54:27 +0530

    Add files via upload

---
+++ digits in factorial of a numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int res=0,num;
+    printf("Enter the number:");
+    scanf("%d",&num);
+    for(int i=1;i<=n;i++)
+    {
+    res+=log(i);
+    printf("%d",res);
+}
